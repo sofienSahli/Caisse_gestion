14b0d6ac39b046211a93661d2908d39108e73cef
sofien <sofien.sahli@esprit.tn>
2018-04-20 13:02:43 +0100

    Drawer up -> Used JFoenex 8.0.2

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,12 +1,66 @@
 package controllers;
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerNextArrowBasicTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
+
+import java.io.IOException;
 
 public class Controller {
+    VBox drawerVbox;
+    @FXML
+    private JFXDrawer drawer;
 
+    @FXML
+    private JFXHamburger hamburger;
 
     @FXML
     public void initialize() {
+        drawerSetting();
+        hambuerSetting();
+    }
+
+    private void drawerSetting() {
+
+        drawerVbox = null;
+        //Drawer VBOX settings
+        try {
+            drawerVbox = FXMLLoader.load(getClass().getResource("../views/DrawerView.fxml"));
+            drawer.setDefaultDrawerSize(Screen.getPrimary().getVisualBounds().getWidth() / 4);
+            ImageView imageView = (ImageView) drawerVbox.getChildren().get(0);
+            imageView.fitWidthProperty().bind(drawerVbox.widthProperty());
+            drawerVbox.setMinWidth(drawer.getDefaultDrawerSize());
+            drawerVbox.setMaxWidth(drawer.getDefaultDrawerSize());
+            drawerVbox.setFillWidth(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        drawer.setSidePane(drawerVbox);
+
+
+    }
+
+    private void hambuerSetting() {
+
+        //Hamburger buttons setting
+        HamburgerNextArrowBasicTransition basicTransition = new HamburgerNextArrowBasicTransition(hamburger);
+        basicTransition.setRate(-1);
+        hamburger.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            basicTransition.setRate(basicTransition.getRate() * -1);
+            basicTransition.play();
+            if (drawer.isHidden()) {
+                drawer.open();
+            } else {
+                drawer.close();
+            }
+        });
 
     }
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,17 +2,22 @@ package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        Scene scene = new Scene(root, screenBounds.getWidth(), screenBounds.getMaxX());
+        scene.getStylesheets().add("views/Main.css");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
